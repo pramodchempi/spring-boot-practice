d19c546991c00aa2273a6f829410a0511a324363
Pramod <pramod.chempi@gmail.com>
2021-06-21 18:31:16 +0530

    Added custom health check and exposed various acuator end points for application.

---
+++ productrestapi/src/main/java/com/pramod/productrestapi/healthchecks/CustomHealthIndicator.java
@@ -0,0 +1,23 @@
+package com.pramod.productrestapi.healthchecks;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health getHealth(boolean includeDetails) {
+        return null;
+    }
+
+    @Override
+    public Health health() {
+        boolean error = false;
+        if(error) {
+            return Health.down().withDetail("Error Key",123).build();
+        }
+        return Health.up().build();
+    }
+}
