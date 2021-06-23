6190d628f992b8f17be84d0600833be2b532aeaa
pramodchempi <34054865+pramodchempi@users.noreply.github.com>
2021-06-23 23:50:13 +0530

    Spring Batch for CSV
    Creating the csv file and storing the values in DB using the spring batch

---
+++ batchcsvtodb/src/main/java/com/pramod/boot/batch/batchcsvtodb/BatchcsvtodbApplication.java
@@ -0,0 +1,13 @@
+package com.pramod.boot.batch.batchcsvtodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchcsvtodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchcsvtodbApplication.class, args);
+	}
+
+}
---
+++ batchcsvtodb/src/test/java/com/pramod/boot/batch/batchcsvtodb/BatchcsvtodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramod.boot.batch.batchcsvtodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchcsvtodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
