477b9410f7149369c4dc4762f2abcda6811a4efa
pramodchempi <34054865+pramodchempi@users.noreply.github.com>
2021-07-08 13:07:48 +0530

    Rest file upload and download
    Application demonstrates rest file upload and download via spring boot.

---
+++ restfileprocessing/src/main/java/com/pramod/rest/fileprocessing/RestfileprocessingApplication.java
@@ -0,0 +1,13 @@
+package com.pramod.rest.fileprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfileprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfileprocessingApplication.class, args);
+	}
+
+}
